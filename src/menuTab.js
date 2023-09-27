d5ad889e3acb9853cbf51f14400414b3c8f81069
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-09-27 19:39:12 -0400

    Finally fixed pics!

--- src/menuTab.js
+++ src/menuTab.js
@@ -1,6 +1,9 @@
+import PastaPic from '/src/images/pasta.jpg';
+import SaladPic from '/src/images/salad.jpg';
+import CannoliPic from '/src/images/cannoli.jpg';
+
 const menuTab = document.createElement("div");
 menuTab.classList.add("tab");
-
 menuTab.setHTML(
   `
   <h2>Discover our Menu</h2>
@@ -11,18 +14,18 @@ menuTab.setHTML(
     <div class="menu-title">Pasta with Red Sauce and Meatballs <span class="menu-price"> - $29.99</span></div>
     <div class="menu-description">Choose between tender spaghetti or spiral rotini, both perfectly paired with our house-special red sauce and succulent meatballs. A rich blend of heirloom tomatoes and aromatic herbs elevate this classic dish to an unforgettable Italian culinary experience.</div>
     
-    <img src="/src/images/pasta.jpg" alt="Pasta with Red Sauce and Meatballs">
+    <img src="${PastaPic}" alt="Pasta with Red Sauce and Meatballs">
   </div>
   <div class="menu-item">
     <div class="menu-title">Italian Salad<span class="menu-price"> - $19.99</span></div>
     <div class="menu-description">Crisp romaine lettuce is tossed with crunchy croutons, juicy tomatoes, tangy pepperoncini, and ripe black olives, then sprinkled with freshly grated Parmesan. A celebration of textures and flavors, this Italian Salad is the perfect start to your meal.</div>
     
-    <img src="/src/images/salad.jpg" alt="Italian Salad">
+    <img src="${SaladPic}" alt="Italian Salad">
   </div>
   <div class="menu-item">
     <div class="menu-title">Cannoli with Rocotta Cheese <span class="menu-price"> - $4.99 Rocotta || $3.50 Choco/Vanilla</span></div>
     <div class="menu-description">Indulge in our iconic Cannoli—crisp pastry shells filled to the brim with creamy ricotta cheese. For a twist, opt for chocolate or vanilla cream. Each bite is a heavenly balance of texture and flavor, making it the perfect ending to your meal.</div>
-    <img src="/src/images/cannoli.jpg" alt="Cannoli with Rocotta Cheese">
+    <img src="${CannoliPic}" alt="Cannoli with Rocotta Cheese">
   </div>
   `)
 
