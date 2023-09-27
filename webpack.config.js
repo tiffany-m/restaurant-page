5af86bc1d9e20e20105cd69edd421c141888b35a
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-09-27 17:18:07 -0400

    Pictures not showing up in build fix

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,12 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                // type: 'asset/resource',
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
             },
         ],
     },
